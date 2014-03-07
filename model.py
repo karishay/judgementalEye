d9d1efac701b0ffcab93ea8c735e8cac2f8669a5
Hackbright Student <user@mini-002.(none)>
2014-03-06 17:35:30 -0800

    Completed part one. We are awesome. Huzzah

--- model.py
+++ model.py
@@ -1,17 +1,74 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+# User
+# id: integer
+# age: integer
+# gender: string
+# zip_code: string (technically zip codes aren't numeric)
+# email: optional string
+# password: optional string
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+    gender = Column(String(10), nullable=True)
 
+# Movie:
+# id: integer
+# name: string
+# released_at: datetime
+# imdb_url: string
+class Movies(Base):
+    __tablename__ = "movies"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(64))
+    released_at = Column(DateTime, nullable=True)
+    imdb_url = Column(String(90), nullable=True)
+
+# Rating:
+# id: integer
+# movie_id: integer
+# user_id: integer
+# rating: integer
+class Rating(Base):
+    __tablename__="rating"
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer)
+    user_id = Column(Integer)
+    rating = Column(Integer)
 
 ### End class declarations
+def createTables():
+    #make a funciton to recreate the tables in the db
+    Base.metadata.create_all(ENGINE)
+    print "all our base are recreated!!!"
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 def main():
     """In case we need this for something"""
-    pass
+    connect()
+    createTables()
+
 
 if __name__ == "__main__":
     main()
--- seed.py
+++ seed.py
@@ -2,21 +2,64 @@ import model
 import csv
 
 def load_users(session):
-    # use u.user
-    pass
+    # open a file
+    with open('seed_data/u.user', 'rb') as csvfile:
+        # parse a line
+        user_file = csv.reader(csvfile, delimiter='|')
+        # read a line
+        for row in user_file:
+            #create a user for each row
+            user = model.User(id=row[0], age=row[1], gender=row[2], zipcode=row[4])
+            #add the user to the session
+            session.add(user)
+        #commit the session to the db
+        #this is important to do in sections to make it ACID compliant-ish
+        session.commit()
+        print "Yay, it added all the users to the DB! :)"
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/u.item', 'rb') as csvfile:
+        movie_file = csv.reader(csvfile, delimiter='|')
+        for row in movie_file:
+            title = row[1]
+            title = title.decode("latin-1")
+            print title
+            movie = model.Movies(id=row[0], name=title, imdb_url=row[3])
+            session.add(movie)
+        session.commit()
+        print "Yay, it added all the movies to the DB!"
+    
 
 def load_ratings(session):
     # use u.data
-    pass
+    with open('seed_data/u.data', 'rb') as csvfile:
+        ratings_file = csv.reader(csvfile, delimiter='\t')
+        for row in ratings_file:
+            ratings = model.Rating(user_id=row[0], movie_id=row[1], rating=row[2])
+            session.add(ratings)
+        session.commit()
+        print "Yay, it added all the ratings to the DB!"
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
+    load_users(session) 
+    #load_movies(session)
+    load_ratings(session) 
     pass
 
 if __name__ == "__main__":
     s= model.connect()
     main(s)
+# open a file
+# read a line
+# parse a line
+# create an object
+# add the object to a session
+# commit
+# repeat until done
+
+#Parsing titles (removing year of release) 
+#Parsing datetimes
+
